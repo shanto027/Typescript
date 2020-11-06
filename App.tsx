b39174e6704c1cbe2598ef3040a3f5eb855b6b9e
shanto027 <shantohalder63@gmail.com>
2020-11-06 22:38:16 +0600

    functional components

--- App.tsx
+++ App.tsx
@@ -1,59 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
-import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, StyleSheet, Button, Text } from 'react-native';
 
 export default function FirstApp() {
 
-  const [varOne, setOne] = useState(0);
-  const [varTwo, setTwo] = useState(0);
-  const [varThree, setThree] = useState(0);
-
-
-  const changeOne=()=>{
-
-    setOne(varOne + 1) ;
-
-    console.log('State One Value = ' + varOne);
-
-  }
-
- const changeTwo=()=>{
-
-    setTwo(varTwo + 1) ;
-
-    console.log('State Two Value = ' + varTwo);
-
-  }
-
-  const changeThree=()=>{
-
-    setThree(varThree + 1) ;
-    console.log('State Three Value = ' + varThree);
-
-  }
-
+  const [count, setCount] = useState(0);
   
   return (
 
     <View style={styleSheet.MainContainer}>
 
-      <TouchableOpacity onPress={changeOne} activeOpacity={0.6} style={styleSheet.button} >
- 
-         <Text style={styleSheet.TextStyle}> Change State One Value </Text>
+      <Text style={styleSheet.text}> Count Value = {count} </Text>
 
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={changeTwo} activeOpacity={0.6} style={styleSheet.button} >
- 
-         <Text style={styleSheet.TextStyle}> Change State Two Value </Text>
-
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={changeThree} activeOpacity={0.6} style={styleSheet.button} >
- 
-         <Text style={styleSheet.TextStyle}> Change State Three Value </Text>
-
-      </TouchableOpacity>
+      <Button onPress={() => setCount(count + 1)} title="Click Here To Change State Value in Functional Component" />
       
     </View>
 
@@ -67,26 +26,12 @@ const styleSheet = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FAFAFA',
-    padding: 10,
+    padding: 10
   },
 
-  button: {
-    
-    width: '80%',
-    paddingTop:5,
-    paddingBottom:5,
-    backgroundColor: '#33691E',
-    borderRadius:5,
-    marginBottom: 20,
-    height: 45,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-   
-  TextStyle:{
-      color:'#fff',
-      textAlign:'center',
-      fontSize: 20
+  text:{
+    fontSize: 20,
+    paddingBottom: 20
   }
  
 });
